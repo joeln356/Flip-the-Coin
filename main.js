4ea202e174531e51795cd05504887da2f55ab454
Joel <joelndala356@gmail.com>
2026-03-22 10:35:47 +0100

    done mobile first

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
 const flip = document.querySelector('.flip');
 const btn = document.getElementById('buttom');
-const text = document.getElementById('texto');
-
+const letra = document.querySelector('.girar')
 
 btn.addEventListener('click', ()=>{
     // Escolhe cara ou coroa aleatoriamente
@@ -9,16 +8,20 @@ btn.addEventListener('click', ()=>{
 
     // Remove rotação anterior
     flip.classList.remove('animar');
+    letra.classList.remove('animar')
 
     // Força reflow para resetar animação
     flip.offsetHeight; // necessário para reiniciar a animação
+    letra.offsetHeight;
 
     // Define rotação baseada no lado
     if(lado === 'cara'){
         flip.style.setProperty('--final-rotation', '2880deg');
+        letra.style.setProperty('--final-rotation', '2880deg');
     } else {
-        flip.style.setProperty('--final-rotation', '3060deg'); 
-
+        flip.style.setProperty('--final-rotation', '3060deg');
+        letra.style.setProperty('--final-rotation', '3060deg');
     }
     flip.classList.add('animar');
+    letra.classList.add('animar')
 });
